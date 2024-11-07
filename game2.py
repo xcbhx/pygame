b37d5eda4ca967a4dc4222daa34059f5412c58c8
xchbx <ceina.ellison@gmail.com>
2024-11-07 13:52:17 -0500

    created drawing on Surface

---
+++ game2.py
@@ -0,0 +1,30 @@
+# Import and initialize pygame
+import pygame
+pygame.init()
+# Configure the screen
+screen = pygame.display.set_mode([500, 500])
+# Creat the game loop
+running = True
+while running:
+  # Looks at events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    # Draw a circle
+    screen.fill((255, 255, 255))
+
+    # Update the window
+    pygame.display.flip()
+
+    # Create a new instance of Surface
+    surf = pygame.Surface((50, 50))
+    surf.fill((255, 111, 33))
+
+    # Clear screen
+    screen.fill((255, 255, 255))
+    # Draw the surface
+    screen.blit(surf, (100, 120))
+
+    # Update the window
+    pygame.display.flip()
