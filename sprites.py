ee5035adeddec009455a44115a43f98b6bb3d925
xchbx <ceina.ellison@gmail.com>
2024-11-12 13:14:04 -0500

    handled events

--- sprites.py
+++ sprites.py
@@ -12,8 +12,6 @@ clock = pygame.time.Clock()
 class GameObject(pygame.sprite.Sprite):
     def __init__(self, x, y, image):
         super(GameObject, self).__init__()
-        # self.surf = pygame.Surface((width, height))
-        # self.surf.fill((255, 0, 255))
         self.surf = pygame.image.load(image)
         self.x = x
         self.y = y
@@ -40,22 +38,44 @@ class Apple(GameObject):
         self.y = -64
 
 
+class Player(GameObject):
+    def __init__(self):
+        super(Player, self).__init__(0, 0, 'player.png')
+        self.dx = 0
+        self.dy = 0
+        self.reset()
+
+    def left(self):
+        self.dx -= 100
+
+    def right(self):
+        self.dx += 100
+
+    def up(self):
+        self.dy -= 100
+
+    def down(self):
+        self.dy += 100
+
+    def move(self):
+        self.x -= (self.x - self.dx) * 0.25
+        self.y -= (self.y - self.dy) * 0.25
+
+    def reset(self):
+        self.x = 250 - 32
+        self.y = 250 - 32
+
 
 #  Instance of GameObject
-# box = GameObject(120, 300, 50, 50)
 apple = GameObject(0, 250, 'apple.png')
-apple1 = GameObject(0, 100, 'apple.png')
-strawberry = GameObject(200, 100, 'strawberry.png')
-apple1 = GameObject(400, 100, 'apple.png')
-strawberry1 = GameObject(0, 250, 'strawberry.png')
-apple2 = GameObject(200, 240, 'apple.png')
-strawberry2 = GameObject(400, 240, 'strawberry.png')
-apple3 = GameObject(0, 400, 'apple.png')
-strawberry3 = GameObject(210, 400, 'strawberry.png')
-apple4 = GameObject(400, 400, 'apple.png')
 
-# Instance of Fruit instances
+
+# Instance of fruit
 apple = Apple()
+
+# Instance of player
+player = Player()
+
 
     # Clear screen
 screen.fill((255, 255, 255))
@@ -67,27 +87,27 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    apple1.render(screen)
-    strawberry.render(screen)
-    apple1.render(screen)
-    strawberry1.render(screen)
-    apple2.render(screen)
-    strawberry2.render(screen)
-    apple3.render(screen)
-    strawberry3.render(screen)
-    apple4.render(screen)
 
-
-    apple.x += 1.8
-    apple.render(screen)
-
+        # Check for event type KEYBOARD
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running == False
+            elif event.key == pygame.K_LEFT:
+                player.left()
+            elif event.key == pygame.K_RIGHT:
+                player.right()
+            elif event.key == pygame.K_UP:
+                player.up()
+            elif event.key == pygame.K_DOWN:
+                player.down()
 
     # Draw apple
     apple.move()
     apple.render(screen)
 
-    # Draw box
-    # box.render(screen)
+    # Draw player
+    player.move()
+    player.render(screen)
 
     # Update the window
     pygame.display.flip()
